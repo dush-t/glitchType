d3b3f9d42c7f017265b2a900faf7c7520c5e709b
Dushyant Yadav <dushyant9309@gmail.com>
2020-03-05 23:22:18 +0530

    Fix keyboard layout

--- glitch_type_client/src/components/Keyboard/Keyboard.js
+++ glitch_type_client/src/components/Keyboard/Keyboard.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
 
 import classes from './Keyboard.module.css'
+import keyboardData from './keyboardData'
 
 import Key from './Key/Key'
 
+
+
 class Keyboard extends Component {
+
     render() {
         return (
             <div className={classes.Keyboard}>
-                <Key pressed={false} keyType={"letter"} keyName={"H"} />
+                <div className={classes.row}>1</div>
+                <div className={classes.row}>2</div>
+                <div className={classes.row}>3</div>
+                <div className={classes.row}>4</div>
+                <div className={classes.row}>5</div>
+                {/* <Key pressed={false} keyType={'letter'} keyName={'H'} /> */}
             </div>
         )
     }
---
+++ glitch_type_client/src/components/Keyboard/keyboardData.js
@@ -0,0 +1,75 @@
+const keyboardData = {
+    row1: [
+        {value: '` ~', type: 'letter', disabled: false}, 
+        {value: '1 !', type: 'letter', disabled: false}, 
+        {value: '2 @', type: 'letter', disabled: false}, 
+        {value: '3 #', type: 'letter', disabled: false}, 
+        {value: '4 $', type: 'letter', disabled: false}, 
+        {value: '5 %', type: 'letter', disabled: false}, 
+        {value: '6 ^', type: 'letter', disabled: false}, 
+        {value: '7 &', type: 'letter', disabled: false}, 
+        {value: '8 *', type: 'letter', disabled: false}, 
+        {value: '9 (', type: 'letter', disabled: false}, 
+        {value: '0 )', type: 'letter', disabled: false}, 
+        {value: '- _', type: 'letter', disabled: false}, 
+        {value: '= +', type: 'letter', disabled: false}, 
+        {value: 'Backspace', type: 'backSpace', disabled: false}
+    ],
+    row2: [
+        {value: 'Tab', type: 'tab', disabled: false}, 
+        {value: 'q', type: 'letter', disabled: false}, 
+        {value: 'w', type: 'letter', disabled: false}, 
+        {value: 'e', type: 'letter', disabled: false}, 
+        {value: 'r', type: 'letter', disabled: false}, 
+        {value: 't', type: 'letter', disabled: false}, 
+        {value: 'y', type: 'letter', disabled: false}, 
+        {value: 'u', type: 'letter', disabled: false}, 
+        {value: 'i', type: 'letter', disabled: false}, 
+        {value: 'o', type: 'letter', disabled: false}, 
+        {value: 'p', type: 'letter', disabled: false}, 
+        {value: '[ {', type: 'letter', disabled: false}, 
+        {value: '] }', type: 'letter', disabled: false}, 
+        {value: '\\ |', type: 'letter', disabled: false}
+    ],
+    row3: [
+        {value: 'Caps', type: 'capsLock', disabled: false}, 
+        {value: 'a', type: 'letter', disabled: false}, 
+        {value: 's', type: 'letter', disabled: false}, 
+        {value: 'd', type: 'letter', disabled: false}, 
+        {value: 'f', type: 'letter', disabled: false}, 
+        {value: 'g', type: 'letter', disabled: false}, 
+        {value: 'h', type: 'letter', disabled: false}, 
+        {value: 'j', type: 'letter', disabled: false}, 
+        {value: 'k', type: 'letter', disabled: false}, 
+        {value: 'l', type: 'letter', disabled: false}, 
+        {value: '; :', type: 'letter', disabled: false}, 
+        {value: '\' "', type: 'letter', disabled: false}, 
+        {value: 'Enter', type: 'enter', disabled: false}
+    ],
+    row4: [
+        {value: 'LShift', type: 'leftShift', disabled: false}, 
+        {value: 'z', type: 'letter', disabled: false}, 
+        {value: 'x', type: 'letter', disabled: false}, 
+        {value: 'c', type: 'letter', disabled: false}, 
+        {value: 'v', type: 'letter', disabled: false}, 
+        {value: 'b', type: 'letter', disabled: false}, 
+        {value: 'n', type: 'letter', disabled: false}, 
+        {value: 'm', type: 'letter', disabled: false}, 
+        {value: ', <', type: 'letter', disabled: false}, 
+        {value: '. >', type: 'letter', disabled: false}, 
+        {value: '/ ?', type: 'letter', disabled: false}, 
+        {value: 'RShift', type: 'rightShift', disabled: false}
+    ],
+    row5: [
+        {value: 'Ctrl', type: 'letter', disabled: true}, 
+        {value: 'Comm', type: 'letter', disabled: true}, 
+        {value: 'rAlt', type: 'letter', disabled: true}, 
+        {value: 'Space', type: 'space', disabled: false}, 
+        {value: 'lAlt', type: 'letter', disabled: true}, 
+        {value: 'Fn', type: 'letter', disabled: true}, 
+        {value: 'Menu', type: 'letter', disabled: true}, 
+        {value: 'Ctrl', type: 'letter', disabled: true}
+    ]
+}
+
+export default keyboardData;
